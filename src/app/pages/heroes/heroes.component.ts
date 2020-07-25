a7222a2d291999c6a77055a7353dbc3fc8575f8c
Yhors Brayan <50890418+Ybrayan@users.noreply.github.com>
2020-07-25 10:35:46 -0500

    Brayan

---
+++ src/app/pages/heroes/heroes.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { HeroeModel } from '../../models/heroe.model';
+import { HttpRequest } from '@angular/common/http';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: HeroeModel[] = [];
+  cargando= false;
+
+  constructor(private heroesService: HeroesService) { }
+
+  ngOnInit(): void {
+
+    this.cargando=true;
+    this.heroesService.getHeroes()
+      .subscribe(resp => {
+        this.heroes = resp;
+        this.cargando=false;
+      });
+  }
+
+  borrarHeroe(heroe: HeroeModel, i: number) {
+    Swal.fire({
+      icon: 'warning',
+      title: '¿Estas Seguro?',
+      text: `Estas seguro que deseas Borrar a ${heroe.nombre}`,
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp => {
+      if (resp.value) {
+        this.heroes.splice(i, 1);
+        this.heroesService.borrarHeroe(heroe.id).subscribe();
+      }
+    })
+  }
+}
